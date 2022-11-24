f27f34ea0f1077342e7c99ba40ddfcd5f866aa29
eti kona <etikonapal@gmail.com>
2022-11-25 05:58:55 +0600

    error page set

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Blogs from "../Pages/Blogs";
+import Error from "../Pages/Error/Error";
 import Home from "../Pages/Home/Home";
 import Login from "../Pages/Login/Login";
 import Signin from "../Pages/Login/Signin";
 
-const router = createBrowserRouter([{
+const router = createBrowserRouter([
+    {
     path: '/',
     element: <Main/>,
     children: [
@@ -26,6 +28,11 @@ const router = createBrowserRouter([{
             element: <Blogs/>
         },
     ]
+},
+
+{
+    path: '*',
+    element: <Error/>
 }
 ]
 )
