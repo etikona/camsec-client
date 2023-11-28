ad114d1f0feac660780d74a1d6d95c5a0cab67d4
eti kona <etikonapal@gmail.com>
2023-11-28 23:07:05 +0600

    site and card background color are changed

--- src/components/Pages/Home/Products/BookingModal.js
+++ src/components/Pages/Home/Products/BookingModal.js
@@ -1,70 +1,110 @@
-import React, { useContext } from 'react';
-import { AuthContext } from '../../../contexts/AuthProvider';
-import toast from 'react-hot-toast';
-import { useNavigate } from 'react-router-dom';
+import React, { useContext } from "react";
+import { AuthContext } from "../../../contexts/AuthProvider";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
 const BookingModal = ({ order }) => {
-    const { name, resellPrice } = order
-    const { user } = useContext(AuthContext);
-    const navigate = useNavigate();
+  const { name, resellPrice } = order;
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
 
+  const handleOrder = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const userName = form.userName.defaultValue;
+    const email = user.email;
+    const price = form.price.defaultValue;
+    const number = form.number.defaultValue;
 
-    const handleOrder = event => {
-        event.preventDefault();
-        const form = event.target;
-        const userName = form.userName.defaultValue;
-        const email = user.email;
-        const price = form.price.defaultValue;
-        const number = form.number.defaultValue;
-        
-        const location = form.location.defaultValue;
-        const orders = {
-            userName, email, price, number, location, name
+    const location = form.location.defaultValue;
+    const orders = {
+      userName,
+      email,
+      price,
+      number,
+      location,
+      name,
+    };
+
+    if (userName && email && price && number && location) {
+      toast.success("Your order place successfully");
+    }
+
+    fetch("https://camsec-server.vercel.app/orders", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(orders),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.acknowledged) {
+          toast("Your order confirmed", {
+            icon: "✔",
+          });
+          navigate("/dashboard");
         }
 
-        if (userName && email && price && number && location) {
-            toast.success("Your order place successfully");
-
-        };
-
-        fetch('https://camsec-server.vercel.app/orders', {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(orders)
-        })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-                if (data.acknowledged) {
-                    toast("Your order confirmed", {
-                        icon: '✔'
-                    })
-                    navigate('/dashboard')
-                }
-                
-                form.reset();
-            })
-    }
-    return (
-        <div>
-            <input type="checkbox" id="my-modal-3" className="modal-toggle" />
-            <div className="modal">
-                <div className="modal-box relative">
-                    <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                    <h3 className="text-lg font-bold">{name}</h3>
-                    <form onSubmit={handleOrder}>
-                        <input type="text"  defaultValue={user.displayName} name='userName' className="input input-bordered w-full max-w-xs" />
-                        <input type="email" readOnly defaultValue={user.email} name='email' className="input input-bordered w-full max-w-xs" />
-                        <input type="text" readOnly defaultValue={resellPrice} name='price' className="input input-bordered w-full max-w-xs" />
-                        <input type="text" placeholder="phone number" name='number' className="input input-bordered w-full max-w-xs" />
-                        <input type="text" placeholder="meeting location" name='location' className="input input-bordered w-full max-w-xs" />
-                        <input type="submit" className='btn btn-outline w-full' value="submit" />
-                    </form>
-                </div>
-            </div>
+        form.reset();
+      });
+  };
+  return (
+    <div>
+      <input type="checkbox" id="my-modal-3" className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box relative">
+          <label
+            htmlFor="my-modal-3"
+            className="btn btn-sm btn-circle absolute right-2 top-2"
+          >
+            ✕
+          </label>
+          <h3 className="text-lg font-bold">{name}</h3>
+          <form onSubmit={handleOrder}>
+            <input
+              type="text"
+              defaultValue={user.displayName}
+              name="userName"
+              className="input input-bordered w-full max-w-xs"
+            />
+            <input
+              type="email"
+              readOnly
+              defaultValue={user.email}
+              name="email"
+              className="input input-bordered w-full max-w-xs"
+            />
+            <input
+              type="text"
+              readOnly
+              defaultValue={resellPrice}
+              name="price"
+              className="input input-bordered w-full max-w-xs"
+            />
+            <input
+              type="text"
+              placeholder="phone number"
+              name="number"
+              className="input input-bordered w-full max-w-xs"
+            />
+            <input
+              type="text"
+              placeholder="meeting location"
+              name="location"
+              className="input input-bordered w-full max-w-xs"
+            />
+            <input
+              type="submit"
+              className="btn btn-outline w-full"
+              value="submit"
+            />
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default BookingModal;
--- src/components/Pages/Home/Products/ProductCard.js
+++ src/components/Pages/Home/Products/ProductCard.js
@@ -1,29 +1,46 @@
-import React from 'react';
+import React from "react";
 
 const ProductCard = ({ product, setOrder }) => {
-    console.log(product);
-    const { name, Location, img, originalPrice
-        , resellPrice, Uses } = product;
-    return (
-        <div>
-            <div className="card w-96 bg-base-100 shadow-xl"><font></font>
-                <figure className="px-10 pt-10"><font></font>
-                    <img src={img} alt="Shoes" className="rounded-xl" /><font></font>
-                </figure><font></font>
-                <div className="card-body items-center text-center"><font></font>
-                    <h2 className="card-title">{name}</h2><font></font>
-                    <p>Location : {Location}</p><font></font>
-                    <p>Original : ${originalPrice}</p><font></font>
-                    <p>Resell : ${resellPrice}</p><font></font>
-                    <p>Used : {Uses}</p><font></font>
-                    <div className="card-actions"><font></font>
+  const { name, Location, img, originalPrice, resellPrice, Uses } = product;
+  return (
+    <div>
+      <div className="card w-96 p-card  shadow-xl">
+        <font></font>
+        <figure className="px-10 pt-10">
+          <font></font>
+          <img src={img} alt="Shoes" className="rounded-xl" />
+          <font></font>
+        </figure>
+        <font></font>
+        <div className="card-body items-center text-center">
+          <font></font>
+          <h2 className="card-title">{name}</h2>
+          <font></font>
+          <p>Location : {Location}</p>
+          <font></font>
+          <p>Original : ${originalPrice}</p>
+          <font></font>
+          <p>Resell : ${resellPrice}</p>
+          <font></font>
+          <p>Used : {Uses}</p>
+          <font></font>
+          <div className="card-actions">
+            <font></font>
 
-                        <label onClick={() => setOrder(product)} htmlFor="my-modal-3" className="btn btn-primary">Book now</label>
-                    </div><font></font>
-                </div><font></font>
-            </div>
+            <label
+              onClick={() => setOrder(product)}
+              htmlFor="my-modal-3"
+              className="btn btn-primary"
+            >
+              Book now
+            </label>
+          </div>
+          <font></font>
         </div>
-    );
+        <font></font>
+      </div>
+    </div>
+  );
 };
 
 export default ProductCard;
